b591bfb5954994aa06e6803ba0f05a4ed7d07ed3
Abhishek Masne <104388375+abhitry@users.noreply.github.com>
2025-03-26 19:49:42 +0530

    Update signup.jsx

--- frontend/src/pages/signup.jsx
+++ frontend/src/pages/signup.jsx
@@ -34,7 +34,7 @@ export const Signup=()=>{
             <div className="pt-4">
                 <Button onClick={async ()=>{
                     try{
-                        const response=await axios.post("http://localhost:3000/api/v1/user/signup",{
+                        const response=await axios.post("https://paytm-backend-1ymy.onrender.com/api/v1/user/signup",{
                         username,
                         firstName,
                         lastName,
