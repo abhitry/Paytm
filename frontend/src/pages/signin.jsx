87477dda345440d864f8facbb3a49c28ec2cf0a1
Abhishek Masne <104388375+abhitry@users.noreply.github.com>
2025-03-26 19:49:10 +0530

    Update signin.jsx

--- frontend/src/pages/signin.jsx
+++ frontend/src/pages/signin.jsx
@@ -25,7 +25,7 @@ export const Signin = () => {
         <div className="pt-4">
         <Button onClick={async ()=>{
             try{
-                    const response=await axios.post("http://localhost:3000/api/v1/user/signin",{
+                    const response=await axios.post("https://paytm-backend-1ymy.onrender.com/api/v1/user/signin",{
                     username,
                     password
                 })
